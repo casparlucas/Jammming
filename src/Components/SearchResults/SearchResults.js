8639201a4b48e8dce38fda00463eb8446b8208a5
casparlucas <88185077+casparlucas@users.noreply.github.com>
2021-12-20 16:00:35 +0000

    Playlist functionality added with renaming & track adding/subtraction

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './SearchResults.css';
 
+import Tracklist from '../Tracklist/Tracklist';
+
 class SearchResults extends React.Component {
     render() {
         return (
         <div className="SearchResults">
             <h2>Results</h2>
-            {/* <Tracklist /> */}
+            <Tracklist tracks={this.props.searchResults} onAdd={this.props.onAdd} isRemoval={false}/>
         </div>)
     }
 }
